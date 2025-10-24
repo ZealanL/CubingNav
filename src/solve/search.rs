327cbe7c65c172acc98d9aa032e51220e35ec6e0
ZealanL <36944229+ZealanL@users.noreply.github.com>
2025-10-24 14:19:18 -0700

    Replace unneeded HashMaps with Vecs in A* search

--- src/solve/search.rs
+++ src/solve/search.rs
@@ -36,34 +36,29 @@ pub fn find_moves(start_state: &CubeState, cost_fn: impl Fn(&CubeState) -> f32, 
         });
     }
 
-    let mut found_states: HashMap<u64, CubeState> = HashMap::from([
-        (start_hash, *start_state)
-    ]);
+    let mut found_states: Vec<CubeState> = vec![*start_state];
+    let mut hash_to_idx_map: HashMap<u64, usize> = HashMap::from([(start_hash, 0)]);
 
-    type OpenSetEntry = (u64, f32);
-
+    type OpenSetEntry = (usize, f32);
     let mut open_set = BinaryHeap::new_by(
         |entry_a: &OpenSetEntry, entry_b: &OpenSetEntry| {
             entry_b.1.partial_cmp(&entry_a.1).unwrap()
         }
     );
-    open_set.push((start_hash, start_cost));
+    open_set.push((0, start_cost));
 
-    let mut parents: HashMap<u64, (u64, AbsCubeMove)> = HashMap::new();
-    let mut g_scores: HashMap<u64, f32> = HashMap::from([
-        (start_hash, 0.0),
-    ]);
-    let mut f_scores: HashMap<u64, f32> = HashMap::from([
-        (start_hash, start_cost),
-    ]);
+    type ParentEntry = Option<(usize, AbsCubeMove)>;
+    let mut parents: Vec<ParentEntry> = vec![None];
+    let mut g_scores: Vec<f32> = vec![0.0];
+    let mut f_scores: Vec<f32> = vec![start_cost];
 
     let start_time = Instant::now();
 
     loop {
-        if let Some((best_open_state_hash, _)) = open_set.pop() {
-            let cur_hash = best_open_state_hash;
-            let cur_state = *found_states.get(&cur_hash).unwrap();
-            let cur_g_score = *g_scores.get(&cur_hash).unwrap_or(&f32::MAX);
+        if let Some((cur_idx, _)) = open_set.pop() {
+
+            let cur_state = found_states[cur_idx];
+            let cur_g_score = g_scores[cur_idx];
             for face in CubeColor::iter() {
                 for turn_dir in [TurnDir::Clockwise, TurnDir::CounterClockwise] {
                     let mv = AbsCubeMove {
@@ -74,22 +69,37 @@ pub fn find_moves(start_state: &CubeState, cost_fn: impl Fn(&CubeState) -> f32, 
                     let neighbor_state = cur_state.turn(mv);
                     let neighbor_hash = neighbor_state.calc_hash();
 
-                    let added_g_score = cur_g_score + 1.0;
+                    // Get or create neighbor's index
+                    let neighbor_idx;
+                    if let Some(existing_neighbor_idx) = hash_to_idx_map.get(&neighbor_hash) {
+                        neighbor_idx = *existing_neighbor_idx;
+                    } else {
+                        neighbor_idx = found_states.len();
+                        found_states.push(neighbor_state);
+                        hash_to_idx_map.insert(neighbor_hash, neighbor_idx);
 
-                    found_states.insert(neighbor_hash, neighbor_state);
+                        // Set initial f/g scores
+                        g_scores.push(f32::MAX);
+                        f_scores.push(f32::MAX);
+
+                        // Set initial parent
+                        parents.push(Some((cur_idx, mv)));
+                    }
+
+                    let added_g_score = cur_g_score + 1.0;
 
                     let neighbor_h_cost = cost_fn(&neighbor_state);
                     let neighbor_f_score = added_g_score + neighbor_h_cost;
-                    let neighbor_g_score = *g_scores.get(&neighbor_hash).unwrap_or(&f32::MAX);
+                    let neighbor_g_score = g_scores[neighbor_idx];
 
                     if neighbor_h_cost <= 0.0 { // Path solved
                         let moves = {
-                            let mut cur_backtrace_hash = cur_hash;
+                            let mut cur_backtrace_idx = cur_idx;
                             let mut moves = vec![mv];
                             loop {
-                                if let Some((prev_hash, prev_turn)) = parents.get(&cur_backtrace_hash) {
-                                    moves.push(*prev_turn);
-                                    cur_backtrace_hash = *prev_hash;
+                                if let Some((parent_idx, prev_turn)) = parents[cur_backtrace_idx] {
+                                    moves.push(prev_turn);
+                                    cur_backtrace_idx = parent_idx;
                                 } else {
                                     break;
                                 }
@@ -109,11 +119,11 @@ pub fn find_moves(start_state: &CubeState, cost_fn: impl Fn(&CubeState) -> f32, 
                     }
 
                     if added_g_score < neighbor_g_score {
-                        parents.insert(neighbor_hash, (cur_hash, mv));
-                        g_scores.insert(neighbor_hash, added_g_score);
-                        f_scores.insert(neighbor_hash, neighbor_f_score);
+                        parents[neighbor_idx] = Some((cur_idx, mv));
+                        g_scores[neighbor_idx] = added_g_score;
+                        f_scores[neighbor_idx] = neighbor_f_score;
 
-                        open_set.push((neighbor_hash, neighbor_f_score));
+                        open_set.push((neighbor_idx, neighbor_f_score));
                         if open_set.len() >= max_explored_states {
                             return Err(SearchError::ExceededLimit);
                         }
